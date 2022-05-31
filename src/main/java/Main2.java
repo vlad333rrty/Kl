db81fecd14abf0a0550a05246f18128e2374bbb4
vlad333rrty <vlad333rrty@yandex-team.ru>
2022-05-31 12:14:19 +0300

    init

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 import kalina.compiler.OxmaCompiler;
 import kalina.compiler.cfg.exceptions.CFGConversionException;
@@ -13,6 +14,7 @@ public class Main2 {
     public static void main(String[] args)
             throws IOException, ParseException, CFGConversionException, CodeGenException, IncompatibleTypesException
     {
+        System.out.print(Arrays.toString(args));
         new OxmaMain(new OxmaCompiler.OxmaCompilerSettings(
                 true,
                 true,
